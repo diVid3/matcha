5977707d4bb2b21faade12fda2e352845420c72d
diVid3 <genisevert7@gmail.com>
2019-10-06 21:46:01 +0200

    made landing responsive

--- assets/landing/landing.js
+++ assets/landing/landing.js
@@ -1,3 +1,22 @@
+// Form switch for login -> register and vice versa.
+const loginForm = document.getElementById('login-form');
+const registerForm = document.getElementById('register-form');
+const loginFormRegister = document.getElementsByClassName('login-form-register')[0];
+const registerFormLogin = document.getElementsByClassName('register-form-login')[0];
+
+loginFormRegister.addEventListener('click', () => {
+  loginForm.classList.toggle('hide-form');
+  registerForm.classList.toggle('hide-form');
+});
+
+registerFormLogin.addEventListener('click', () => {
+  registerForm.classList.toggle('hide-form');
+  loginForm.classList.toggle('hide-form');
+});
+
+// TODO: Front-end UX validation.
+// TODO: Backend registration rejection.
+
 // Register form constants.
 // const firstNme = document.getElementById('first-name');
 // const lastName = document.getElementById('last-name');
